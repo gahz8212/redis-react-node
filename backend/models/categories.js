103d1580e01e75e9b22c63243f11d3f57ab18c21
kimsunghyun <berserk.gahz@gmail.com>
2026-01-06 05:26:33 +0900

    restore models

---
+++ backend/models/categories.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+module.exports = class Categories extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        category: {
+          type: Sequelize.STRING(50),
+          allowNull: false,
+        },
+       
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "Category",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.Category.belongsTo(db.Photo);
+    db.Category.belongsTo(db.PhotoCategoryMap);
+
+  }
+};
---
+++ backend/models/emotionstargets.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+module.exports = class EmotionsTargets extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        satisfaction: {
+          //만족여부
+          type: Sequelize.STRING(1),
+          allowNull: false,
+        },
+        target: {
+          type: Sequelize.STRING(100),
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: "EmotionsTarget",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
+  }
+  static associate(db) {
+    db.EmotionsTarget.belongsTo(db.Trip);
+    db.EmotionsTarget.belongsTo(db.Photo);
+  }
+};
