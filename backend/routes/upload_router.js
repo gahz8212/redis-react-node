1822e95e2bea4fecd83c4fdd8e574da33d701364
sung hyun Kim <97937963+gahz8212@users.noreply.github.com>
2026-01-28 18:51:55 +0900

    add resize upload_router.js

--- backend/routes/upload_router.js
+++ backend/routes/upload_router.js
@@ -5,6 +5,35 @@ const albumService = require("../db/upload_db.js");
 router.post("/", upload.single("image"), async (req, res) => {
   const { tripId } = req.body;
   console.log("파일 수신 완료:", req.file.filename);
+   try {
+    // 1. 로그인 여부 확인 (Passport가 제공하는 함수)
+    if (!req.isAuthenticated())
+      return res.status(401).json({ error: "로그인이 필요합니다." });
+    // 2. 파일 유효성 검사
+    if (!req.file) {
+      return res.status(400).json({ error: "파일 없음" });
+    }
+    // 원본 파일 경로
+    const inputPath = req.file.path;
+    const outputPath = path.join(
+      "uploads",
+      Date.now() + "-" + req.file.originalname,
+    );
+    // sharp으로 리사이즈 및 압축
+    await sharp(inputPath)
+      .resize({ width: 1024 }) // 가로 1024px, 세로는 비율 유지
+      .jpeg({ quality: 80 }) // JPEG 품질 70%
+      .toFile(outputPath);
+    // 원본 삭제 → 압축본만 남김
+    fs.unlinkSync(inputPath);
+    // req.file 정보를 압축본으로 교체
+    req.file.path = outputPath;
+    req.file.filename = path.basename(outputPath);
+    const result = await albumService.uploadProcess(
+      req.user.id,
+      tripId,
+      req.file.filename,
+    );
   const result = await albumService.uploadProcess(
     req.user.id,
     tripId,
