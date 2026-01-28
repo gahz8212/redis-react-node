b8b69b663cae5cd0a1142cc387f040f01ccb9c0a
kimsunghyun <berserk.gahz@gmail.com>
2026-01-28 13:26:45 +0900

    update on_local

---
+++ backend/db/upload_db.js
@@ -0,0 +1,19 @@
+// server/db_layer/user_db.js
+const pool = require("./db");
+/**
+ * username + password로 사용자 조회
+ * - 로그인용
+ * - 반환: { id, username, createdAt } 또는 null
+ */
+async function uploadProcess(userId, tripId, photo) {
+  const [rows] = await pool.query(
+    "insert into photos (photo,UserId,TripId) values (?,?,?)",
+    [photo, userId, tripId],
+  );
+  if (!rows || rows.length === 0) return null;
+  return rows[0];
+}
+
+module.exports = {
+  uploadProcess,
+};
